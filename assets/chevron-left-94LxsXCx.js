c29b24a59aef58d32148ef8fae5268692931b0fe
Doni Wahyudi <doni.wahyudi@btpnsyariah.com>
2026-05-30 12:51:41 +0700

    Updates

---
+++ assets/chevron-left-94LxsXCx.js
@@ -0,0 +1,1 @@
+import{u as e}from"./index-Bl0N4FDL.js";var t=e(`chevron-left`,[[`path`,{d:`m15 18-6-6 6-6`,key:`1wnfg3`}]]);export{t};
---
+++ assets/chevron-right-BaWGvQe4.js
@@ -0,0 +1,1 @@
+import{u as e}from"./index-Bl0N4FDL.js";var t=e(`chevron-right`,[[`path`,{d:`m9 18 6-6-6-6`,key:`mthhwq`}]]);export{t};
